802f3b489170d87003aed37491833fb74ba33246
luckyqiang <1595541621@qq.com>
2020-06-15 20:47:31 +0800

    init ..

---
+++ src/cn/luckyqiang/test/arraylist/TestArrayList.java
@@ -0,0 +1,21 @@
+package cn.luckyqiang.test.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: TestArrayList
+ * @Description: TODO
+ * @Author: zhangzhiqiang
+ * @Date: 2020-06-15 20:22
+ * @Company: www.luckyqiang.cn
+ */
+public class TestArrayList {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+
+    }
+}
---
+++ src/cn/luckyqiang/test/hashmap/TestHashMap.java
@@ -0,0 +1,11 @@
+package cn.luckyqiang.test.hashmap;
+
+/**
+ * @ClassName: TestHashMap
+ * @Description: TODO
+ * @Author: zhangzhiqiang
+ * @Date: 2020-06-15 20:22
+ * @Company: www.luckyqiang.cn
+ */
+public class TestHashMap {
+}
